5422b37eecffe08041c351e490cf3d7145957024
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-06-13 15:46:28 +0800

    hal_cmake: add enet related support
    Added support for enet, enet-qos, mdio, phy and ARMv8-A Cache.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- mcux/hal_nxp.cmake
+++ mcux/hal_nxp.cmake
@@ -123,7 +123,8 @@ include_driver_ifdef(CONFIG_DMA_MCUX_EDMA		dmamux		driver_dmamux)
 include_driver_ifdef(CONFIG_ENTROPY_MCUX_RNGA		rnga		driver_rnga)
 include_driver_ifdef(CONFIG_ENTROPY_MCUX_TRNG		trng		driver_trng)
 include_driver_ifdef(CONFIG_ENTROPY_MCUX_CAAM		caam		driver_caam)
-include_driver_ifdef(CONFIG_ETH_MCUX			enet		driver_enet)
+include_driver_ifdef(CONFIG_HAS_MCUX_ENET		enet		driver_enet)
+include_driver_ifdef(CONFIG_HAS_MCUX_ENET_QOS		enet_qos	driver_enet_qos)
 include_driver_ifdef(CONFIG_HAS_MCUX_I2S		sai		driver_sai)
 include_driver_ifdef(CONFIG_HAS_MCUX_II2C		ii2c		driver_ii2c)
 include_driver_ifdef(CONFIG_HAS_MCUX_SMC		smc		driver_smc)
@@ -178,6 +179,7 @@ include_driver_ifdef(CONFIG_MCUX_SDIF			sdif		driver_sdif)
 include_driver_ifdef(CONFIG_ADC_MCUX_ETC		adc_etc		driver_adc_etc)
 include_driver_ifdef(CONFIG_MCUX_XBARA			xbara		driver_xbara)
 include_driver_ifdef(CONFIG_QDEC_MCUX			enc		driver_enc)
+include_driver_ifdef(CONFIG_ARMV8A_CACHE_HAL_DRIVER	cache/armv8-a	driver_cache_armv8a)
 
 if ((${MCUX_DEVICE} MATCHES "MIMXRT1[0-9][0-9][0-9]") AND (NOT (CONFIG_SOC_MIMXRT1166_CM4 OR CONFIG_SOC_MIMXRT1176_CM4)))
   include_driver_ifdef(CONFIG_HAS_MCUX_CACHE		cache/armv7-m7	driver_cache_armv7_m7)
@@ -323,3 +325,35 @@ include_codec_ifdef(CONFIG_CODEC_PCM512X	pcm512x)
 include_codec_ifdef(CONFIG_CODEC_PCM186X	pcm186x)
 include_codec_ifdef(CONFIG_CODEC_WM8960 	wm8960)
 include_codec_ifdef(CONFIG_CODEC_WM8524 	wm8524)
+
+if(CONFIG_PHY_RTL8211F)
+  list(APPEND CMAKE_MODULE_PATH
+    ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy
+    ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy/device/phyrtl8211f
+  )
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy)
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy/device/phyrtl8211f)
+  include(driver_phy-device-rtl8211f)
+endif()
+
+if(CONFIG_MDIO_ENET)
+  list(APPEND CMAKE_MODULE_PATH
+    ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy
+    ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy/mdio/enet
+  )
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy)
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy/mdio/enet)
+  include(driver_mdio-common)
+  include(driver_mdio-enet)
+endif()
+
+if(CONFIG_MDIO_ENET_QOS)
+  list(APPEND CMAKE_MODULE_PATH
+    ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy
+    ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy/mdio/enet_qos
+  )
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy)
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/components/phy/mdio/enet_qos)
+  include(driver_mdio-common)
+  include(driver_mdio-enet-qos)
+endif()
