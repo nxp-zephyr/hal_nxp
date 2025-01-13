b121aba4b6d764687d0b9fbcc29a33844050e7ab
Emilio Benavente <emilio.benavente@nxp.com>
2025-01-13 15:27:26 -0600

    boards: nxp: Update Device name
    After JLink V8.12A Update,
    The MCXW72 device has official
    support under the name MCXW727
    
    Signed-off-by: Emilio Benavente <emilio.benavente@nxp.com>

--- boards/nxp/mcxw72_evk/board.cmake
+++ boards/nxp/mcxw72_evk/board.cmake
@@ -1,6 +1,6 @@
 # Copyright 2024 NXP
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=MCXW72B" "--reset-after-load")
+board_runner_args(jlink "--device=MCXW727" "--reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
