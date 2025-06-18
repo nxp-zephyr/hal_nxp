6be069293e8fcbe669d725a7ab07578d7c5a8874
Winteri Wang <dongjie.wang@nxp.com>
2025-06-18 20:44:43 +0800

    mcux: driver: lcdifv3: extra update for lcdifv3 driver
    Signed-off-by: Winteri Wang <dongjie.wang@nxp.com>

--- mcux/mcux-sdk/drivers/lcdifv3/fsl_lcdifv3.h
+++ mcux/mcux-sdk/drivers/lcdifv3/fsl_lcdifv3.h
@@ -353,7 +353,7 @@ static inline void LCDIFV3_ClearInterruptStatus(LCDIF_Type *base, uint32_t mask)
  */
 static inline void LCDIFV3_SetLayerSize(LCDIF_Type *base, uint8_t layerIndex, uint16_t width, uint16_t height)
 {
-    base->CTRLDESCL_1 =
+    base->CTRLDESCL_1[0] =
         ((uint32_t)height << LCDIF_CTRLDESCL_1_HEIGHT_SHIFT) | ((uint32_t)width << LCDIF_CTRLDESCL_1_WIDTH_SHIFT);
 }
 
@@ -377,7 +377,7 @@ void LCDIFV3_SetLayerBufferConfig(LCDIF_Type *base, uint8_t layerIndex, const lc
  */
 static inline void LCDIFV3_SetLayerBufferAddr(LCDIF_Type *base, uint8_t layerIndex, uint32_t addr)
 {
-    base->CTRLDESCL_LOW_4 = LCDIFV3_ADDR_CPU_2_IP(addr);
+    base->CTRLDESCL_LOW_4[0] = LCDIFV3_ADDR_CPU_2_IP(addr);
 }
 
 /*!
@@ -392,11 +392,11 @@ static inline void LCDIFV3_EnableLayer(LCDIF_Type *base, uint8_t layerIndex, boo
 {
     if (enable)
     {
-        base->CTRLDESCL_5 |= LCDIF_CTRLDESCL_5_EN_MASK;
+        base->CTRLDESCL_5[0] |= LCDIF_CTRLDESCL_5_EN_MASK;
     }
     else
     {
-        base->CTRLDESCL_5 &= ~LCDIF_CTRLDESCL_5_EN_MASK;
+        base->CTRLDESCL_5[0] &= ~LCDIF_CTRLDESCL_5_EN_MASK;
     }
 }
 
@@ -414,7 +414,7 @@ static inline void LCDIFV3_EnableLayer(LCDIF_Type *base, uint8_t layerIndex, boo
  */
 static inline void LCDIFV3_TriggerLayerShadowLoad(LCDIF_Type *base, uint8_t layerIndex)
 {
-    base->CTRLDESCL_5 |= LCDIF_CTRLDESCL_5_SHADOW_LOAD_EN_MASK;
+    base->CTRLDESCL_5[0] |= LCDIF_CTRLDESCL_5_SHADOW_LOAD_EN_MASK;
 }
 
 /*!
@@ -428,6 +428,8 @@ static inline void LCDIFV3_TriggerLayerShadowLoad(LCDIF_Type *base, uint8_t laye
  */
 void LCDIFV3_SetCscMode(LCDIF_Type *base, lcdifv3_csc_mode_t mode);
 
+void LCDIFV3_EnablePlanePanic(LCDIF_Type *base);
+
 /*! @} */
 
 #if defined(__cplusplus)
@@ -437,3 +439,4 @@ void LCDIFV3_SetCscMode(LCDIF_Type *base, lcdifv3_csc_mode_t mode);
 /*! @} */
 
 #endif /*FSL_LCDIFV3_H_*/
+
