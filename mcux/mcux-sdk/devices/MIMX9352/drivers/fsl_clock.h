b741ba3d894a51e9d987c025b1e815b035b22729
Zoey Li <zoey.li@nxp.com>
2025-07-15 14:00:06 +0900

    devices: MIMX9352: fsl_clock: add missing definitions for ISI
    Add clock array for ISI.
    
    Signed-off-by: Zoey Li <zoey.li@nxp.com>

--- mcux/mcux-sdk/devices/MIMX9352/drivers/fsl_clock.h
+++ mcux/mcux-sdk/devices/MIMX9352/drivers/fsl_clock.h
@@ -1112,6 +1112,12 @@ typedef enum _clock_lpcg
         kCLOCK_IpInvalid, kCLOCK_Sema1, kCLOCK_Sema2 \
     }
 
+/*! @brief Clock ip name array for ISI. */
+#define ISI_CLOCKS  \
+    {               \
+        kCLOCK_Isi  \
+    }
+
 /*******************************************************************************
  * Clock Root APIs
  ******************************************************************************/
